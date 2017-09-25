a1fb8e3f888d1deadcdb6277b6f84eff71e88d12
Aditya Arora <adityaarora@v1020-wn-184-159.campus-dynamic.uwaterloo.ca>
2017-09-25 13:17:06 -0400

    Tried to fix according to new update, still unresolved

--- LAB2/ECBraking.cpp
+++ LAB2/ECBraking.cpp
@@ -16,36 +16,49 @@ using namespace std;
 int main(const int argc, const char* const argv[])
 {
   float v = atof(argv[1]), d = atof(argv[2]), max_acc = atof(argv[3]);
-  float acc = v*v/(2*d);
+  float acc = fabs(v*v/(2*d));
 
   if(argc > 4){
-    cerr << "Warning: Expecting three command-line arguments; received greater than three; ignoring extraneous arguments." << endl;
-    return -1;
+    //     Warning: Expecting three command-line arguments; received ...; ignoring extraneous arguments
+    cerr<<"Warning: Expecting three command-line arguments; received "<<argc-1<<"; ignoring extraneous arguments" << endl;
   }
   else if (argc < 4){
-    cerr << "Warning: Expecting exactly three command-line arguments;" << endl;
+    cerr<<"Error: Expecting exactly three command-line arguments;" << endl;
+    return -1;
   }
-  else if((v > 0 && d < 0) || (v < 0 && d > 0)){
-    cout<< "There is no possibility of collision because the velocity is in the opposite direction of distance\n";
+  else if (max_acc < 0){
+    cerr<<"Error: Only positive accelaration allowed;\n";
+    return -1;
+  }
+  if((v > 0 && d < 0) || (v < 0 && d > 0)){
+    cout<<"There is no possibility of collision because the velocity is in the opposite direction of distance\n";
   }
   else if(v == 0){
-    cout<<"There is no possibility of collision because "<<"the velocity is zero\n";
+    cout<<"There is no possibility of collision because the velocity is zero\n";
   }
   else if(acc <= max_acc){
     cout<<"Initial car velocity: "<<v<<" m/s"<<endl;
     cout<<"Distance to obstacle: "<<d<<" m"<<endl;
-    cout<<"Minimum deceleration: "<<acc<<" m/s/s"<<endl;
-    cout<<"Maximum stopping time: "<<v/acc<<" s"<<endl;
+    cout<<"Minimum deceleration: "<<fabs(acc)<<" m/s/s"<<endl;
+    cout<<"Maximum stopping time: "<<fabs(v/acc)<<" s"<<endl;
   }
   else if(max_acc == 0){
-    cout<<"Crash will occur in "<<d/v<<" seconds at velocity "<<v<<" m/s; deploying airbags.\n";
+    cout<<"Crash will occur in "<<fabs(d/v)<<" seconds at velocity "<<v<<"m/s; deploying airbags.\n";
   }
-  else{
-    acc = max_acc;
-    float u = pow(abs(pow(v,2)-(2*acc*d)), 0.5);
-    //     Crash will occur in      ...        seconds at velocity   ...  m/s; deploying airbags.
-    //Crash will occur in ... seconds at velocity ...m/s; deploying airbags.
-    cout<<"Crash will occur in "<<(v-u)/acc<<" seconds at velocity "<<u<<" m/s; deploying airbags.\n";
+  else if(acc > max_acc){
+    if(d > 0){
+      acc = max_acc;
+      float u = pow(fabs(pow(v,2)-(2*acc*d)), 0.5)*(v/fabs(v));
+      //    'Crash will occur in ... seconds at velocity ...m/s; deploying airbags.'
+      cout<<"Crash will occur in "<<fabs((v-u)/acc)<<" seconds at velocity "<<u<<"m/s; deploying airbags.\n";
+    }
+    else{
+      acc = max_acc;
+      float u = pow(fabs(pow(v,2)+(2*acc*d)), 0.5)*(v/fabs(v));
+      //    'Crash will occur in ... seconds at velocity ...m/s; deploying airbags.'
+      cout<<"Crash will occur in "<<fabs((v-u)/acc)<<" seconds at velocity "<<u<<"m/s; deploying airbags.\n";
+    }
+
   }
 
   return 0;

ca878c4970fe95714ddc1ae3245b90053c895264
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2017-10-30 10:49:38 -0400

    Added MOSS for fun

---
+++ moss.py
@@ -0,0 +1,21 @@
+import mosspy
+import sys
+
+userid = 680980157
+
+m = mosspy.Moss(userid, "cc")
+
+# Submission Files
+m.addFile(str(sys.argv[1]))
+m.addFile(str(sys.argv[2]))
+
+
+url = m.send() # Submission Report URL
+
+print ("Report Url: " + url)
+
+# # Save report file
+# m.saveWebPage(url, "submission/report.html")
+#
+# # Download whole report locally including code diff links
+# mosspy.download_report(url, "submission/report/", connections=8)

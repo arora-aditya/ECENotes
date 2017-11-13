2f0e127d0463dff05c86b02fa39692d8a0155131
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2017-11-13 11:28:48 -0500

    Finished StringToFloat

--- LAB7/StringToFloat.cpp
+++ LAB7/StringToFloat.cpp
@@ -12,71 +12,85 @@
 
 using namespace std;
 
-bool stringToFloat(const char input[], float& value){
+bool stringToFloat(const char input[], float& value) {
     float result = 0;
     int sign = 1;
-	int i = 0;
-	if(input[i] == '-' || input[i] == '+') sign = (input[i++] == '-')? -1 : 1;
-	while('0' <= input[i] && input[i] <= '9'){
-		result = result*10 + (input[i++]-'0');
-        if(result*sign >= 3.40282e+38){
-			return false;
-		}
-        if(result*sign <= -3.40282e+38){
-			return false;
-		}
-	}
+    int i = 0;
+    if(input[0] == '\0') {
+        return false;
+    }
+    if(input[i] == '-' || input[i] == '+') sign = (input[i++] == '-')? -1 : 1;
+    while('0' <= input[i] && input[i] <= '9') {
+        result = result*10 + (input[i++]-'0');
+        if(result*sign >= 3.40282e+38) {
+            return false;
+        }
+        if(result*sign <= -3.40282e+38) {
+            return false;
+        }
+    }
     float j = 0.1;
-    if(input[i] == '.'){
+    if((input[i] == '.' && (input[i-1] == '-' || input[i-1] == '+') && !('0' <= input[i+1] && input[i+1] <= '9')) || (input[i] == '.' && input[i+1] == '\0')) {
+        return false;
+    }
+    if(input[i] == '.') {
         i++;
-        while('0' <= input[i] && input[i] <= '9'){
+        while('0' <= input[i] && input[i] <= '9') {
             result = result + ((input[i++] - '0') * (j));
             j/=10;
         }
     }
     int po = 0, k = 0, signpo = 1;
     float p = 1;
-    if(input[i] == 'e'){
+    if((input[i] == 'e' || input[i] == 'E') && (input[i-1] == '.' || input[i+1] == '\0')) {
+        return false;
+    }
+    if((input[i] == 'e' || input[i] == 'E') && i == 0) {
+        return false;
+    }
+    if(input[i] == 'e' || input[i] == 'E') {
         i++;
         if(input[i] == '-' || input[i] == '+') signpo = (input[i++] == '-')? -1 : 1;
-        while('0' <= input[i] && input[i] <= '9'){
-            po = po*10 + (input[i++]-'0'); 
+        while('0' <= input[i] && input[i] <= '9') {
+            po = po*10 + (input[i++]-'0');
         }
         po = po * signpo;
-        if(po > 0){
-            while(k < po){
+        if(po > 0) {
+            while(k < po) {
                 p *= 10;
                 k++;
             }
         }
-        else{
-            while(k > po){
+        else {
+            while(k > po) {
                 p /= 10;
                 k--;
             }
-        }    
+        }
     }
     result *= p;
-	if(input[i] != '\0'){
-		return false;
-	}
-	value = sign * result;
-	return true;
+    if(input[i] != '\0') {
+        return false;
+    }
+    value = sign * result;
+    return true;
 }
 
+#ifndef MARMOSET_TESTING
 int main()
 {
-    char input[][100] = {"","123.456","+.123456","+0.123456","-123.456","+123.456e+7","123.456e7","-123.456e-7","-000123.456e0007","-.+", "+.","..","+.e9","+1.e-9","-123.-987e-3","-123.987e-3","0.00000000000000001357924681031415","13579246810314159265"};
+    char input[][100] = {"e1","123247932we","S","3.0000000000000000090384590384509843580938405895","","12.","12.e1","123","1e7","1.-1e-5","0.1e5","123.456","+.123456","+.123456e-9","+0.123456","-123.456","+123.456e+00007","+123.+7","123.456e7","-123.456e-7","-000123.456e0000","-.+", "+.","..","+.e9","+.e","+1.e-100","-123.-987e-3","-123.987e-3","-123.987e-3e4","0.00000000000000001357924681031415","13579246810314159265"};
     float value = 0;
     int size = sizeof(input)/sizeof(input[0]);
-    for(int i = 0; i < size; i++){
-        if(stringToFloat(input[i], value)){
-            cout<<"the number is :"<<value<<endl;
+    for(int i = 0; i < size; i++) {
+        if(stringToFloat(input[i], value)) {
+            cout<<"the number is "<<input[i]<<" : "<<value<<endl;
         }
-        else{
-            cout<<"Error"<<endl;
+        else {
+            cout<<"Error: "<<input[i]<<endl;
         }
     }
 
-	return 0;
+    return 0;
 }
+#endif
